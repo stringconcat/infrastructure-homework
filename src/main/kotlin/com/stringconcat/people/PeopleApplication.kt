07de6ce1e0dfffc89b0dcc5bed3f2ee2d2a246f0
gaverianov <gregory.averianov@gmail.com>
2022-07-12 11:45:47 +0300

    commit

--- src/main/kotlin/com/stringconcat/people/PeopleApplication.kt
+++ src/main/kotlin/com/stringconcat/people/PeopleApplication.kt
@@ -2,8 +2,10 @@ package com.stringconcat.people
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(value = ["com.stringconcat.people"])
 class PeopleApplication
 
 fun main(args: Array<String>) {
