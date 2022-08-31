f8274a2e7d3990b8b47136279924f053c7f20d9a
Sergey Kolosov <sergey.kolosov@mvideo.ru>
2022-08-31 11:26:26 +0300

    add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,32 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.3"
-    id("io.spring.dependency-management") version "1.0.13.RELEASE"
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10"
+    id("org.springframework.boot") version "2.7.3"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.7.10" apply false
     id("io.gitlab.arturbosch.detekt") version "1.21.0"
+    id("org.owasp.dependencycheck") version "7.1.1"
+    id("com.github.ben-manes.versions") version "0.42.0"
+    jacoco
 }
 
 allprojects {
+
     group = "com.stringconcat"
-    version = "0.0.1"
+
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "org.owasp.dependencycheck")
+    apply(plugin = "com.github.ben-manes.versions")
+    apply(plugin = "jacoco")
+    apply(plugin = "base")
+
 
     repositories {
         mavenCentral()
         maven { url = uri("https://repo.spring.io/milestone") }
         maven { url = uri("https://repo.spring.io/snapshot") }
-    }
-
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "1.8"
-            allWarningsAsErrors = true
-        }
     }
 
     configurations.all {
@@ -44,7 +45,13 @@ allprojects {
         }
     }
 
-    apply(plugin = "io.gitlab.arturbosch.detekt")
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+            allWarningsAsErrors = true
+        }
+    }
 
     detekt {
         buildUponDefaultConfig = true
@@ -52,22 +59,63 @@ allprojects {
         config = files("$rootDir/detekt/config.yml")
     }
 
+    jacoco {
+        toolVersion = "0.8.7"
+    }
+
     tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        jvmTarget = "1.8"
         reports {
             html.required.set(true)
         }
     }
 
-    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-        jvmTarget = "1.8"
+    tasks {
+
+        val validateDependencies = named<DefaultTask>("check")
+        val dependencyUpdate = named<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask>("dependencyUpdates")
+
+        dependencyUpdate {
+            revision = "release"
+            outputFormatter = "txt"
+            checkForGradleUpdate = true
+            outputDir = "$buildDir/reports/dependencies"
+            reportfileName = "updates"
+        }
+
+        dependencyUpdate.configure {
+
+            fun isNonStable(version: String): Boolean {
+                val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+                val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+                val isStable = stableKeyword || regex.matches(version)
+                return isStable.not()
+            }
+
+            rejectVersionIf {
+                isNonStable(candidate.version) && !isNonStable(currentVersion)
+            }
+        }
+
+        dependencyCheck {
+            failBuildOnCVSS = 2f
+            cveValidForHours = 3
+        }
+
+
+        validateDependencies {
+            finalizedBy(dependencyUpdate)
+        }
+
     }
+
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 
-
 dependencies {
+
     // spring modules
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -99,8 +147,20 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
 }
 
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
 
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
