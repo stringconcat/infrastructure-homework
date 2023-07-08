0de94bff467ec6e99bee32ce621905b81bcfc0cb
Daniil Sagalov <danielsagel@outlook.com>
2023-07-08 14:04:12 +0300

    STRCNCT-0001 - Added JUnit 5 to business people dependencies for businessPeople in build.gradle.kts.

--- businessPeople/build.gradle.kts
+++ businessPeople/build.gradle.kts
@@ -14,3 +14,7 @@ dependencies {
     testCompile("org.junit.jupiter:junit-jupiter-api:5.5.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
