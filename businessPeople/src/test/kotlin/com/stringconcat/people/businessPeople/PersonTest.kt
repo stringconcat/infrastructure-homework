d6afd4164d259b5ccbbc346dafc2ed86780d9c42
Daniil Sagalov <danielsagel@outlook.com>
2023-07-08 14:22:08 +0300

    STRCNCT-0001 - PersonTest.kt description formatting

--- businessPeople/src/test/kotlin/com/stringconcat/people/businessPeople/PersonTest.kt
+++ businessPeople/src/test/kotlin/com/stringconcat/people/businessPeople/PersonTest.kt
@@ -20,50 +20,57 @@ class PersonTest {
 
 
     @Test
-    fun `Person created valid`() {
+    fun `should create correct person when valid parameters provided`() {
         // given
+        val personId = UUID.fromString("29f4d7e3-fd7c-4664-ad07-763326215ec4")
+        val firstName = "Evgeniy"
+        val lastName = "Lukyanov"
+        val birthDate = LocalDate.of(1987,12,1)
+        val personSex = Person.Sex.MAN
+        val favoriteQuote = "make the easy things easy, and the hard things possible"
+
+        // when
         person = Person(
-            id = UUID.fromString("29f4d7e3-fd7c-4664-ad07-763326215ec4"),
-            firstName = "Evgeniy",
-            secondName = "Lukyanov",
-            birthDate = LocalDate.of(1987,12,1),
-            sex = Person.Sex.MAN,
-            favoriteQuote = "make the easy things easy, and the hard things possible"
+            id = personId,
+            firstName = firstName,
+            secondName = lastName,
+            birthDate = birthDate,
+            sex = personSex,
+            favoriteQuote = favoriteQuote
         )
 
         // then
         assertAll(
-            Executable { assertEquals("29f4d7e3-fd7c-4664-ad07-763326215ec4", person.id.toString()) },
-            Executable { assertEquals("Evgeniy", person.firstName) },
-            Executable { assertEquals("Lukyanov", person.secondName) },
-            Executable { assertEquals(LocalDate.of(1987,12,1), person.birthDate) },
-            Executable { assertEquals(Person.Sex.MAN, person.sex) },
-            Executable { assertEquals("make the easy things easy, and the hard things possible",
-                person.favoriteQuote) },
+            Executable { assertEquals(personId.toString(), person.id.toString()) },
+            Executable { assertEquals(firstName, person.firstName) },
+            Executable { assertEquals(lastName, person.secondName) },
+            Executable { assertEquals(birthDate, person.birthDate) },
+            Executable { assertEquals(personSex, person.sex) },
+            Executable { assertEquals(favoriteQuote, person.favoriteQuote) },
             Executable { assertEquals(DEFAULT_ROBOT_AVATAR, person.avatartUrl) }
         )
     }
 
     @Test
-    fun `Person age check - not mature`() {
+    fun `should return mature false when age less than 40`() {
         // then
         assertFalse(person.mature(DATE_OF_PROJECT))
     }
 
     @Test
-    fun `Person age check - mature`() {
+    fun `should return mature true when age bigger than 40`() {
         // given
-        person = person.copy(birthDate = LocalDate.of(
-            /* year = */ 1950,
-            /* month = */ 12,
-            /* dayOfMonth = */ 1))
+        val personBirthDate =  LocalDate.of(/* year = */ 1950, /* month = */ 12, /* dayOfMonth = */ 1)
+
+        // when
+        person = person.copy(birthDate = personBirthDate)
 
         // then
         assertTrue(person.mature())
     }
 
     @Test
-    fun `Change avatar works`() {
+    fun `should change person avatar when changeAvatar(String) called`() {
         // when
         person.changeAvatar(DEFAULT_ROBOT_AVATAR)
 
@@ -72,9 +79,16 @@ class PersonTest {
     }
 
     @Test
-    fun `Age calculated properly for person`() {
+    fun `should calculate age properly when age(LocalDate) called`() {
+        // given
+        val yearAfterBirth = person.birthDate.plusYears(1)
+        val expectedAgeAfterBirth = 1
+
+        // when
+        val actualAgeAfterBirth = person.age(yearAfterBirth)
+
         // then
-        assertEquals(1, person.age(person.birthDate.plusYears(1)))
+        assertEquals(expectedAgeAfterBirth, actualAgeAfterBirth)
     }
 
     private fun defaultPerson(): Person {
